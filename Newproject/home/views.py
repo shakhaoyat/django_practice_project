44e15ee9a7712a491282d82a68a3f8f09a632dc1
shakhaoyat <shakhaoyathossain.12@gmail.com>
2025-01-09 23:59:30 +0600

    new

--- Newproject/home/views.py
+++ Newproject/home/views.py
@@ -5,7 +5,14 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,"demo.html")
+    dict_user=[]
+    x={"Name":"Shakhaoyat","Email":shakhaoyat@xyz.com}
+    dict_user.append(x)
+    x={"Name":"Hossain","Email":Hossain@xyz.com}
+    dict_user.append(x)
+    x={"Name":"Chad","Email":Chad@xyz.com}
+    dict_user.append(x)
+    return render(request,"demo.html",{"Users":dict_user})
 def login(request):
     return render(request,"login.html")
 def signup(request):
