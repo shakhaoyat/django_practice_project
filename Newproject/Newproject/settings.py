81330e230ef48cdd6a1109d479bdb47c82afa74f
shakhaoyat <shakhaoyathossain.12@gmail.com>
2025-01-30 23:24:13 +0600

    20th day

--- Newproject/Newproject/settings.py
+++ Newproject/Newproject/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL= '/media/'
+STATIC_ROOT =os.path.join(BASE_DIR,"static_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media_cnd")
+STATICFILES_DIRS=[
+      os.path.join(BASE_DIR,"static"),
+      os.path.join(BASE_DIR,"media"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
